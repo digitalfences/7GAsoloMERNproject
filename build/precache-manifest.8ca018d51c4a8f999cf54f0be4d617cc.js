c3e4587fa94821bb87bdf25efc2e23800a726f8d
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-04-20 04:01:48 -0400

    build ready?

---
+++ build/precache-manifest.8ca018d51c4a8f999cf54f0be4d617cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90d277476c1746cb70c772a16db8430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c3e6ffdccfb5e29174d",
+    "url": "/static/css/main.f72eb9d2.chunk.css"
+  },
+  {
+    "revision": "d15182f4715e5d98d4dc",
+    "url": "/static/js/2.ab99c35a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ab99c35a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c3e6ffdccfb5e29174d",
+    "url": "/static/js/main.114a1a4f.chunk.js"
+  },
+  {
+    "revision": "d9a2df771ef5bda69662",
+    "url": "/static/js/runtime-main.6b30d221.js"
+  }
+]);
